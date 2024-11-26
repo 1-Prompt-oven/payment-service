48f402d2c4165bdde8995eaff5a950e37c2a71c8
jaehunLee-git <joz526@naver.com>
2024-11-26 10:56:01 +0900

    Feat : 카프카 파티션 및 레플리카 설정 자동화

---
+++ src/main/java/com/promptoven/paymentservice/global/config/KafkaTopicConfig.java
@@ -0,0 +1,30 @@
+package com.promptoven.paymentservice.global.config;
+
+import org.apache.kafka.clients.admin.AdminClientConfig;
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.KafkaAdmin;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class KafkaTopicConfig {
+
+    @Value("${spring.kafka.text-bootstrap-servers}")
+    private String bootstrapServers;
+
+    @Bean
+    public KafkaAdmin kafkaAdmin() {
+        Map<String, Object> configs = new HashMap<>();
+        configs.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        return new KafkaAdmin(configs);
+    }
+
+    @Bean
+    public NewTopic myTopic() {
+        return new NewTopic("payment-create-event", 5, (short) 2); // 파티션 5개, 복제본 2개
+    }
+}
