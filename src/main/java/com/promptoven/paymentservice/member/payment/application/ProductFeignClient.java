68850454a3d5d3e3b26e8b1f71432850f8a15971
jaehunLee-git <joz526@naver.com>
2024-12-04 09:34:56 +0900

    Refactor : Feign Client 엔드 포인트 수정

--- src/main/java/com/promptoven/paymentservice/member/payment/application/ProductFeignClient.java
+++ src/main/java/com/promptoven/paymentservice/member/payment/application/ProductFeignClient.java
@@ -6,9 +6,9 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "product-service", url = "http://localhost:8080")
+@FeignClient(name = "product-service", url = "https://api.promptoven.shop")
 public interface ProductFeignClient {
 
-    @GetMapping("/v1/payments/product/seller/test/{productUUID}")
+    @GetMapping("/v1/product/{productUUID}/seller")
     BaseResponse<ProductResponseDto> getProductByUuid(@PathVariable("productUUID") String productUUID);
 }
